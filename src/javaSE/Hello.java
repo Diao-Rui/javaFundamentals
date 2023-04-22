1985569cf7beef4289a27fe3336df1c90c25a903
dg <2438747008@qq.com>
2023-04-22 12:40:14 +0800

    message

---
+++ src/javaSE/Hello.java
@@ -0,0 +1,15 @@
+package javaSE;
+
+/**
+ * @Author Diao Rui
+ * @Date 2023/4/21 17:47
+ * @PackageName:javaSE
+ * @ClassName: Hello
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
