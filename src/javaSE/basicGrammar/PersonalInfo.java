623c58eac9e95b91e6ed5ebb4416f143d360c905
dg <2438747008@qq.com>
2023-04-24 17:56:35 +0800

    message

---
+++ src/javaSE/basicGrammar/PersonalInfo.java
@@ -0,0 +1,12 @@
+package javaSE.basicGrammar;
+
+/**
+ * @Author Diao Rui
+ * @Date 2023/4/24 10:54
+ * @PackageName:javaSE.basicGrammar
+ * @ClassName: PersonalInfo
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class PersonalInfo {
+}
