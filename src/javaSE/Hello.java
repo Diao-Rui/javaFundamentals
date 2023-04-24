175544cc8fdc8d597faa2ad1077fe10431c9a88f
dg <2438747008@qq.com>
2023-04-24 17:56:44 +0800

    message

--- src/javaSE/Hello.java
+++ src/javaSE/Hello.java
@@ -11,5 +11,6 @@ package javaSE;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Integer a;
     }
 }
--- src/javaSE/basicGrammar/PersonalInfo.java
+++ src/javaSE/basicGrammar/PersonalInfo.java
@@ -9,4 +9,9 @@ package javaSE.basicGrammar;
  * @Version 1.0
  */
 public class PersonalInfo {
+    public static void main(String[] args){
+        System.out.println("姓名：康师傅");
+        System.out.println("性别：男");
+        System.out.println("家庭住址：北京程序员集结地：回龙坝");
+    }
 }
