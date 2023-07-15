616ca863c690b12c97a4646d2cb784bc411f7e80
diaorui <xdgzst010@163.com>
2023-07-15 10:00:48 +0800

    mesage

---
+++ src/javaSE/advance/Reflection/Bird.java
@@ -0,0 +1,12 @@
+package javaSE.advance.Reflection;
+
+/**
+ * @Author Diao Rui
+ * @Date 2023/7/11 10:49
+ * @PackageName:javaSE.advance.Reflection
+ * @ClassName: Bird
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class Bird {
+}
